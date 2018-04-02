ebf52a927b99cb2ef76ebb41b39c80c043634087
Sang Won Han <petershan0315@gmail.com>
2018-04-03 08:05:55 +0900

    EC2 Private IP 설정

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -10,6 +10,33 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '.elasticbeanstalk.com',
 ]
+
+def is_ec2_linux():
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen("http://169.254.169.254/latest/meta-data/local-ipv4")
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+        ALLOWED_HOSTS.append(private_ip)
+
+        
 WSGI_APPLICATION = 'config.wsgi.production.application'
 INSTALLED_APPS += [
     'storages',
